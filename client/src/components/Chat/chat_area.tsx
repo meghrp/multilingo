220c7e7cb1cdd2909556c0a64433799c8401dc87
meghpathak <meghrp27@gmail.com>
2024-05-09 21:14:22 -0400

    Added basic chat functionality using SignalR library

--- client/src/components/Chat/chat_area.tsx
+++ client/src/components/Chat/chat_area.tsx
@@ -1,9 +1,47 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { SentMessage, ReceivedMessage } from "./chat_messages";
+import * as signalR from "@microsoft/signalr";
+import React, { useState, useEffect } from "react";
 
 export function ChatArea() {
+	const username = new Date().getTime();
+	const [message, setMessage] = useState("");
+	const [chatMessages, setChatMessages] = useState<Array<JSX.Element>>([]);
+	const [connection, setConnection] = useState<signalR.HubConnection | null>(null);
+
+	useEffect(() => {
+		const newConn = new signalR.HubConnectionBuilder().withUrl("http://localhost:5204/hub").build();
+		setConnection(newConn);
+	}, []);
+
+	useEffect(() => {
+		if (connection) {
+			connection
+				.start()
+				.then(() => console.log("Connection started!"))
+				.catch((err) => {
+					console.error(err);
+				});
+
+			connection.on("messageReceived", (username: string, message: string) => {
+				const element = <ReceivedMessage message={message} time={username.toString()} />;
+				setChatMessages((prev) => [...prev, element]);
+			});
+		}
+	}, [connection]);
+
+	function send() {
+		if (connection) {
+			connection.send("newMessage", username, message).then(() => console.log("Message sent!"));
+			setChatMessages((prev) => [...prev, <SentMessage message={message} time={username.toString()} />]);
+			setMessage("");
+		}
+	}
+
 	return (
 		<div className="flex-1 bg-white dark:bg-gray-950 border-r border-gray-200 dark:border-gray-800 p-4 flex flex-col">
 			{/* Header */}
@@ -27,20 +65,33 @@ export function ChatArea() {
 
 			{/* Messages */}
 			<div className="flex-1 overflow-y-auto">
-				<div className="flex flex-col gap-4">
-					<ReceivedMessage message="Hello, how are you?" time="12:34 PM" />
+				<div className="flex flex-col gap-4" id="messages">
+					{/* <ReceivedMessage message="Hello, how are you?" time="12:34 PM" />
 					<SentMessage message="I'm doing great, thanks for asking!" time="12:35 PM" />
-					<ReceivedMessage message="Did you see the new design?" time="12:36 PM" />
+					<ReceivedMessage message="Did you see the new design?" time="12:36 PM" /> */}
+					{chatMessages.map((message, index) => (
+						<React.Fragment key={index}>{message}</React.Fragment>
+					))}
 				</div>
 			</div>
 
 			{/* Textarea */}
 			<div className="mt-4 flex items-center gap-2">
-				<Input className="flex-1" placeholder="Type your message..." />
+				<Input
+					className="flex-1"
+					type="text"
+					placeholder="Type your message..."
+					id="messageInput"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					onKeyUp={(e) => {
+						if (e.key === "Enter") send();
+					}}
+				/>
 				<Button size="icon" variant="ghost">
 					<PaperclipIcon className="h-5 w-5" />
 				</Button>
-				<Button size="icon" variant="ghost">
+				<Button size="icon" variant="ghost" type="submit" onClick={send} id="btnSend">
 					<SendIcon className="h-5 w-5" />
 				</Button>
 			</div>
