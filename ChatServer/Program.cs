cb8e69a773625e60604ed94d89297ffa4ac507a1
meghpathak <meghrp27@gmail.com>
2024-05-17 15:07:54 -0400

    updated server files to use minimal APIs

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,18 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-namespace ChatServer
-{
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+app.MapGet("/", () => "Hello World!");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.Run();
