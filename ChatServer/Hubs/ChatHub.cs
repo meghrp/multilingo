8c40d5489f47c333fe56fe147e30c79ce62eccc9
meghpathak <meghrp27@gmail.com>
2024-06-05 15:13:55 -0400

    removed .net files

--- ChatServer/Hubs/ChatHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-namespace ChatServer.Hubs;
-
-public class ChatHub : Hub
-{
-    public async Task NewMessage(long username, string message) =>
-        await Clients.Others.SendAsync("messageReceived", username, message);
-}
