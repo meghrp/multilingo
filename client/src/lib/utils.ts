97c6a421b5aca2b6c0b5a718c25cb083d4493e02
meghpathak <meghrp27@gmail.com>
2025-03-06 00:36:31 -0500

    feat: add utility function for merging class names

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines multiple class values into a single className string
+ * Uses clsx for conditional classes and tailwind-merge to handle conflicting Tailwind classes
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
