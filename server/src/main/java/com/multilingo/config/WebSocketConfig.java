9cac729bf1b45f629269ac859d87777fc2a50ac1
meghpathak <meghrp27@gmail.com>
2025-03-07 11:52:46 -0500

    updated websocket config - added jwt stuff

--- server/src/main/java/com/multilingo/config/WebSocketConfig.java
+++ server/src/main/java/com/multilingo/config/WebSocketConfig.java
@@ -3,7 +3,10 @@ package com.multilingo.config;
 import static org.springframework.util.MimeTypeUtils.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -12,21 +15,36 @@ import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
 import java.util.List;
 
+/**
+ * WebSocket configuration
+ */
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 98)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Bean
+    public HandshakeInterceptor jwtHandshakeInterceptor() {
+        return new JwtHandshakeInterceptor();
+    }
+    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/user");
+        registry.enableSimpleBroker("/user", "/topic");
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .addInterceptors(jwtHandshakeInterceptor())
+                .withSockJS();
     }
 
     @Override
